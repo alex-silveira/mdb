39c688a35ff7c89636d526d6c50f19135fb4e4b6
alex-silveira <alexarauj0015@hotmail.com>
2025-05-27 21:24:49 -0300

    add function replaceAllItems

--- src/data/database.ts
+++ src/data/database.ts
@@ -4,11 +4,10 @@ const db = openDatabaseSync('spreedsheet.db');
 
 import { Item } from '@/models/Item'
 
-
 export const createTable = () => {
 db.execAsync(`
     CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER,
         name TEXT,
         price TEXT
     );
@@ -16,10 +15,15 @@ db.execAsync(`
   };
 ;
 
-export const insertItem = async (name: string, price: string) => {
+export const dropTable = () => {
+db.execAsync(`DROP TABLE products`);
+  };
+;
+
+export const insertItem = async (id: number, name: string, price: string) => {
   await db.runAsync(
-    'INSERT INTO products (name, price) VALUES (?, ?)',
-    name, price
+    'INSERT INTO products (id, name, price) VALUES (?, ?, ?)',
+    id, name, price
   );
 };
 
@@ -27,14 +31,34 @@ export const selectItems = async (): Promise<Item[]> => {
   const result = await db.getAllAsync('SELECT * FROM products');
 
   return result.map((item: any) => ({
-    id: Number(item.id), // <- Converte para número
+    id: Number(item.id), 
     name: String(item.name),
     price: String(item.price),
   }));
 };
 
 export const clearItems = () => {
-  db.runAsync('DELETE FROM items;');
+  db.runAsync('DELETE FROM products;');
 }
+
+export const replaceAllItems = async (dados: [number, string, string][]) => {
+  await db.withTransactionAsync(async () => {
+    await db.runAsync('DELETE FROM products;');
+
+    for (const item of dados) {
+      const id = item[0] || 'Sem Id';
+      const name = item[1] || 'Sem Nome';
+      const price = item[2] || 'Sem Preço';
+
+      await db.runAsync(
+        'INSERT INTO products (id, name, price) VALUES (?, ?, ?);',
+        [id, name, price]
+      );
+    }
+  });
+
+  console.log('Dados substituídos com sucesso!');
+};
+
 
 export default db;
